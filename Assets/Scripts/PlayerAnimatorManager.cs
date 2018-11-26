f9c31032e06c782289320387b8804400ae4d513c
masonentrican <mason@entrican.com>
2018-11-25 16:21:39 -0800

    Staging to swap to Unity 5.6.0 for UFPS support

---
+++ Assets/Scripts/PlayerAnimatorManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.MasonEntrican.MultiplayerFramework
+{
+    public class PlayerAnimatorManager : MonoBehaviour
+    {
+
+        private Animator animator;
+
+        #region MonoBehaviour Callbacks
+
+        
+
+        // Use this for initialization
+        void Start()
+        {
+            //animator = GetComponent<Animator>();
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        #endregion
+    }
+}
+
